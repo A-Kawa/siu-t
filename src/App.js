a6fcc8dbecb614965e90cb9244d0ad87e01e50cd
Andrzej <kawus9@gmail.com>
2019-02-22 16:52:02 +0200

    Added eslintrc

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1> start </h1>
+        <div className="grid-left">
+          <div className="left-one">
+            <h1>Test</h1>
+          </div>
+        </div>
+        <div className="grid-right">
+          <div className="right-one">
+            <h1>Test2</h1>
+          </div>
+        </div>
       </div>
     );
   }
